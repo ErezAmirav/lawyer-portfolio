2724ff7e65c1a4027a0f66bce784c1da99b5fdee
Erez <erezmirav@gmail.com>
2023-07-13 18:18:20 +0300

    updated about section

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -1,18 +1,46 @@
 import React from 'react';
-import './About.css'
+import './About.css';
+import AboutImg from '../../assets/img/lawyer-about.jpg'
 const About = () => {
   return (
     <div className="about_container">
-      <h1>
-        Our Team of Skilled With <br />
-        Extensive Knowledge.
-      </h1>
-      <p>
-        With a strong reputation for excellence and a track record of successful
-        outcomes, we are here to protect your rights.
+      <div className="content-container">
+        <h1>
+          Our Team of Skilled With <br />
+          Extensive Knowledge.
+        </h1>
+        <p>
+          Whether You Are Facing A Challenging Legal Dispute,
+          <br /> Seeking Guidance In A Business Matter, We Are Here To Help.
+        </p>
+      </div>
+      <div className="boxes">
+        <div className="box">
+          <h2 className="box-title">Civil Litigation</h2>
+          <p className="box-text">
+            We Assist Individuals And Businesses In Safeguarding Their Creative
+            Works
+          </p>
+        </div>
+        <div className="box">
+          <h2 className="box-title">Estate Planning</h2>
+          <p className="box-text">
+            Your Wishes Are Honored With Our Through Estate Planning Services
+          </p>
+        </div>
+        <div className="box">
+          <h2 className="box-title">Intellectual Property</h2>
+          <p className="box-text">
+            Businesses In Safeguarding Their Creative Works And Trade Secrets.
+          </p>
+        </div>
+      </div>
+      <h1>Your Trusted Legal Advisors</h1>
+      <p className='second-text-info'>
+        Our Team Of Skilled Attorneys Combines Legal Knowledge
+        <br /> With A Commitment To Delivering Results.
       </p>
-      <button>Call Now</button>
-      <p>Our Achieved Awards</p>
+      <img src={AboutImg} alt=""/>
     </div>
   );
 };

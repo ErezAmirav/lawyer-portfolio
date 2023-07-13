10bf25563fcf75af76058673490d68624938c268
Erez <erezmirav@gmail.com>
2023-07-13 14:44:39 +0300

    header + navbar fix

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,16 +1,27 @@
 import React from 'react';
-import './Header.css'
+import './Header.css';
+import LawyerImg from '../../assets/img/lawyer-header.jpg';
+
 const Header = () => {
   return (
-    <div className='header_container'>
-      <h1>Unbelievable Solutions <br/>
-      For All Legal Cases</h1>
-      <p>
-        With a strong reputation for excellence and a track record of successful
-        outcomes, we are here to protect your rights.
-      </p>
-      <button>Call Now</button>
-      <p>Our Achieved Awards</p>
+    <div className="header_container">
+      <div className="content-container">
+        <div className="text-container">
+          <h1>
+            Unbelievable Solutions <br />
+            For All Legal Cases
+          </h1>
+          <p className='intro'>
+            With a strong reputation for excellence and a track record of
+            successful outcomes, we are here to protect your rights.
+          </p>
+          <button className='header-btn'>Call Now</button>
+          <p className='achiev'>Our Achieved Awards</p>
+        </div>
+        <div className="image-container">
+          <img src={LawyerImg} alt="" className="header-image" />
+        </div>
+      </div>
     </div>
   );
 };
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -3,15 +3,17 @@ import './Navbar.css';
 
 const Navbar = () => {
   return (
-    <nav className="nav">
-      <span className='nav-title'>Lawyers Inc.</span>
-      <ul>
-        <li>Home</li>
-        <li>About</li>
-        <li>Contact</li>
-      </ul>
-      <button>Call Now</button>
-    </nav>
+    <div className="nav-container">
+      <nav className="nav">
+        <span className="nav-title">Lawyers Inc.</span>
+        <ul>
+          <li>Home</li>
+          <li>About</li>
+          <li>Contact</li>
+        </ul>
+        <button>Call Now</button>
+      </nav>
+    </div>
   );
 };
 

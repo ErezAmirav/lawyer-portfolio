a0d874a9761415f4f8cf9582a523101aea855aaa
Erez <erezmirav@gmail.com>
2023-07-25 17:47:05 +0300

    added navbar + header responibility

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -7,29 +7,38 @@ import Reward2 from '../../assets/img/reward2.png';
 const Header = () => {
   return (
     <header>
-    <div className="header_container">
-      <div className="content-container">
-        <div className="text-container">
-          <h1>
-            Unbelievable Solutions <br />
-            For All Legal Cases
-          </h1>
-          <p className="intro">
-            With a strong reputation for excellence and a track record of
-            successful outcomes, we are here to protect your rights.
-          </p>
-          <button className="main-button header-btn">Call Now</button>
-          <p className="achiev">Our Achieved Awards</p>
-          <div className="rewards">
-            <img src={Reward1} alt="" className="reward reward1" />
-            <img src={Reward2} alt="" className="reward reward2" />
+      <div className="header_container">
+        <div className="content-container">
+          <div className="text-container">
+            <h1>
+              Unbelievable Solutions <br />
+              For All Legal Cases
+            </h1>
+            <div className="desktopIntro">
+              <p className="intro">
+                With a strong reputation for excellence and a track record of
+                successful outcomes, we are here to protect your rights.
+              </p>
+              <button className="main-button header-btn">Call Now</button>
+            </div>
+            <p className="achiev">Our Achieved Awards</p>
+            <div className="rewards">
+              <img src={Reward1} alt="" className="reward reward1" />
+              <img src={Reward2} alt="" className="reward reward2" />
+            </div>
           </div>
-        </div>
-        <div className="image-container">
-          <img src={LawyerImg} alt="" className="header-image" />
+          <div className="image-container">
+            <img src={LawyerImg} alt="" className="header-image" />
+          </div>
+          <div className="text-container mobileIntro">
+              <p className="intro">
+                With a strong reputation for excellence and a track record of
+                successful outcomes, we are here to protect your rights.
+              </p>
+              <button className="main-button header-btn">Call Now</button>
+            </div>
         </div>
       </div>
-    </div>
     </header>
   );
 };
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,25 +1,50 @@
 import React from 'react';
+import { useRef } from 'react';
 import './Navbar.css';
+import { FaBars, FaTimes } from 'react-icons/fa';
+
 
 const Navbar = () => {
+  const navRef = useRef();
+  const showNavbar = () => {
+    navRef.current.classList.toggle('responsive_nav');
+  };
+
+  const hideNavbar =()=>{
+
+    navRef.current.classList.remove("responsive_nav")
+
+  }
+
+
   return (
     <div className="nav-container">
-      <nav className="nav">
-        <span className="nav-title">Lawyers Inc.</span>
+      <nav className="nav" ref={navRef}>
+        <h3 className="nav-title">Lawyers Inc.</h3>
         <ul>
           <li>
-            <a href="#">Home</a>
+            <a href="#" onClick={hideNavbar}>Home</a>
           </li>
           <li>
-            <a href="#about">About</a>
+            <a href="#about" onClick={hideNavbar}>About</a>
           </li>
           <li>
-            <a href="#reviews">Reviews</a>
+            <a href="#reviews" onClick={hideNavbar}>Reviews</a>
           </li>
           <li>Contact</li>
         </ul>
-        <button className='main-button'>Call Now</button>
+        <button className='main-button nav-call-btn'>Call Now</button>
+        <button
+        className="nav-btn nav-close-btn"
+        onClick={showNavbar}>
+        <FaTimes />
+      </button>
       </nav>
+      <button
+      className="nav-btn"
+      onClick={showNavbar}>
+      <FaBars />
+    </button>
     </div>
   );
 };
